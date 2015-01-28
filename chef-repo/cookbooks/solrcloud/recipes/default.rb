afc8c1552222690e81bb0371793f3021d159a271
Abby Hahn <abigail.n.hahn@gmail.com>
2015-01-28 14:50:07 -0800

    Created new cookbooks for SolrCloud virtualization

---
+++ chef-repo/cookbooks/solrcloud/recipes/default.rb
@@ -0,0 +1,21 @@
+#
+# Cookbook Name:: solrcloud
+# Recipe:: default
+#
+# Copyright 2015, YOUR_COMPANY_NAME
+#
+# All rights reserved - Do Not Redistribute
+#
+
+# Install necessary packages
+
+execute "yum update && yum -y groupinstall \"Development Tools\" \"Development Libraries\""
+
+# Install Ruby and dependencies
+
+# Install knife-container gem
+
+# Install Docker
+
+
+
