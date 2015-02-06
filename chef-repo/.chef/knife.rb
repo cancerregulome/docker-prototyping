220ecd8b7f8475b42bf00dc93efdc03901e2a8d7
Abby Hahn <abigail.n.hahn@gmail.com>
2015-02-06 11:37:01 -0800

    Fixed some bugs in the workstation setup scripts, added new entries to .gitignore, and deleted some scratch directories.

--- chef-repo/.chef/knife.rb
+++ chef-repo/.chef/knife.rb
@@ -1,9 +1,9 @@
 log_level                :info
 log_location             STDOUT
-node_name                'admin'
-client_key               '/home/abby/docker-prototyping/chef-repo/.chef/admin.pem'
-validation_client_name   'isb-validator'
-validation_key           '/home/abby/docker-prototyping/chef-repo/.chef/isb-validator.pem'
-chef_server_url          'https://chef-server-centos.com/organizations/isb'
-syntax_check_cache_path  '/home/abby/docker-prototyping/chef-repo/.chef/syntax_check_cache'
-cookbook_path [ '/home/abby/docker-prototyping/chef-repo/cookbooks' ]
+node_name                "#{ENV['admin_client']}"
+client_key               "#{ENV['chef_repo']}/.chef/#{ENV['admin_client']}.pem"
+validation_client_name   "#{ENV['validator']}"
+validation_key           "#{ENV['chef_repo']}/.chef/#{ENV['validator']}.pem"
+chef_server_url          "https://#{ENV['chef_server']}/organizations/isb"
+syntax_check_cache_path  "#{ENV['chef_repo']}/.chef/syntax_check_cache"
+cookbook_path [ "#{ENV['chef_repo']}/cookbooks" ]
