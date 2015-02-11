38cd9e0c007d22b9d34d9c78bd42ae51f7b37c79
Abby Hahns <ahahn@systemsbiology.org>
2015-02-11 13:39:47 -0800

    Finished writing  the vm setup scripts, but have yet to test them.

--- setup/vm_setup/vbox_env.sh
+++ setup/vm_setup/vbox_env.sh
@@ -3,10 +3,16 @@
 # The directory where all of the ISO files live
 #export iso_dir=/Users/ahahn/Desktop/Stuff
 
+# The name of the .ova file to import
+#export cluster_base=Cluster_Base.ova
+
 # The directory where VirtualBox stores vms, by default
 #export vbox_dir="/Users/ahahn/VirtualBox VMs/"
 
 # The directory functioning as a shared volume between VMs and the host
 #export vbox_share=/Users/ahahn/vm_guest_config
+
+# Add VBoxManage to the system path
+#export PATH=..:$PATH
 
 
--- setup/vm_setup/vm_cluster_setup.sh
+++ setup/vm_setup/vm_cluster_setup.sh
@@ -6,38 +6,74 @@
 # 3) Install guest additions CD-ROM drive for base image in VirtualBox
 # 4) Log in again, and in the directory where the cd rom drive is mounted run "./VBoxLinuxAdditions.run"; logout.
 
-
 # First, set up the environment
 
 source vbox_env.sh
 
+vbox_network_create () {
+	# Create the network
+	VBoxManage natnetwork add --netname NatNetwork1 --enable --dhcp on
+	VBoxManage hostonlyif --dhcp create vboxnet0 
+	VBoxManage dhcpserver add --ip 192.168.56.100 --netmask 255.255.255.0 --lowerip 192.168.56.101 --upperip 192.168.56.254 --enable
+}
 
-# Import the cluster base image for the chef server and nodes, add a shared folder containing server setup scripts, and configure the servers
-VBoxManage import $iso_dir/Cluster_Base.ova --vsys 0 --vmname "Chef Server Centos"
-VBoxManage sharedfolder add "Chef Server Centos" --name "vm_setup_scripts" --hostpath $vbox_share --transient --automount
-VBoxManage startvm "Chef Server Centos" --type headless
-VBoxManage guestcontrol execute --image /media/sf_vm_setup_scripts/fix_networking.sh
-VBoxManage guestcontrol execute --image /media/sf_vm_setup_scripts/chef_server.sh
-# May need to wait before the poweroff?
-VBoxManage controlvm "Chef Server Centos" poweroff
+chef_server_create () {
+	# Import the cluster base image for the chef server and nodes, add a shared folder containing server setup scripts, and configure the servers
+	VBoxManage import $iso_dir/Cluster_Base.ova --vsys 0 --vmname "Chef Server Centos"
+	VBoxManage sharedfolder add "Chef Server Centos" --name "vm_setup_scripts" --hostpath $vbox_share --transient --automount
+	VBoxManage startvm "Chef Server Centos" --type headless
+	VBoxManage guestcontrol execute --image /media/sf_vm_setup_scripts/fix_networking.sh
+	VBoxManage guestcontrol execute --image /media/sf_vm_setup_scripts/chef_server.sh
+	# May need to wait before the poweroff?
+	VBoxManage controlvm "Chef Server Centos" poweroff
+}
 
-VBoxManage import $iso_dir/Cluster_Base.ova --vsys 0 --vmname "SolrCloud 1"
-VBoxManage sharedfolder add "SolrCloud1" --name "vm_setup_scripts" --hostpath $vbox_share --transient --automount
-VBoxManage startvm "SolrCloud1" --type headless
-VBoxManage guestcontrol execute --image /media/sf_vm_setup_scripts/fix_networking.sh
-VBoxManage controlvm "SolrCloud1" poweroff
+chef_node_create () {
+	node_name=ChefNode$1
+	VBoxManage import $iso_dir/$cluster_base --vsys 0 --vmname $node_name
+	VBoxManage sharedfolder add $node_name --name "vm_setup_scripts" --hostpath $vbox_share --transient --automount
+	VBoxManage startvm $node_name --type headless
+	VBoxManage guestcontrol $node_name execute --image /media/sf_vm_setup_scripts/fix_networking.sh
+	VBoxManage guestcontrol $node_name execute --image /media/sf_vm_setup_scripts/fix_hosts_file.sh
+	VBoxManage controlvm $node_name poweroff
+}
+# Ask the user if they want to create a new network
+read -p "Would you like to create a new local network for your Virtualbox cluster? [Y/N] " yn
+while true; do
+	case $yn in
+		Y|y)
+			network_create;;
+		N|n)
+			break;;
+		*)
+	esac
+done
 
-VBoxManage import $iso_dir/Cluster_Base.ova --vsys 0 --vmname "SolrCloud 2"
-VBoxManage sharedfolder add "SolrCloud2" --name "vm_setup_scripts" --hostpath $vbox_share --transient --automount
-VBoxManage startvm "SolrCloud2" --type headless
-VBoxManage guestcontrol execute --image /media/sf_vm_setup_scripts/fix_networking.sh
-VBoxManage controlvm "SolrCloud2" poweroff
+# Ask the user if they want to create a new chef server
+read -p "Would you like to create a new chef server? [Y/N] " yn
 
-VBoxManage import $iso_dir/Culster_Base.ova --vsys 0 --vmname "SolrCloud 3"
-VBoxManage sharedfolder add "SolrCloud3" --name "vm_setup_scripts" --hostpath $vbox_share --transient --automount
-VBoxManage startvm "SolrCloud3" --type headless
-VBoxManage guestcontrol execute --image /media/sf_vm_setup_scripts/fix_networking.sh
-VBoxManage controlvm "SolrCloud3" poweroff
+while true; do
+	case $yn in
+		Y|y)
+			chef_server_create;;
+		N|n)
+			break;;
+		*)
+	esac
+done
+
+read -p "How many chef nodes would you like to create? " node_num
+
+if [[ -z "$node_num" ]]; then
+	read -p "Please enter a number of nodes greater or equal to zero: " node_num
+else
+	count=0
+
+	while [[ $count < $node_num ]]; do
+		chef_node_create $node_num
+		((count += 1))
+	done
+fi
 
 
 
