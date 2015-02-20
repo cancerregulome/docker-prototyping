fd75b9376f72ad2baed7e7f243624350aae528c3
vardaofthevalier <abigail.n.hahn@gmail.com>
2015-02-19 18:51:49 -0800

    Reorganized the setup_scripts directory, and updated .gitignore

--- test_infrastructure_setup/setup_scripts/linux/vbox/vbox_network_setup.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-# Create the network
-VBoxManage natnetwork add --netname NatNetwork1 --enable --dhcp on
-VBoxManage hostonlyif --dhcp create vboxnet0 
-VBoxManage dhcpserver add --ip 192.168.56.100 --netmask 255.255.255.0 --lowerip 192.168.56.101 --upperip 192.168.56.254 --enable
