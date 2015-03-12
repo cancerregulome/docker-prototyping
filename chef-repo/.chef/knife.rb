f00a8afe4250d2ab99ea6ead7d102ba4bb209334
Abby Hahn <abigail.n.hahn@gmail.com>
2015-03-11 18:51:10 -0700

    fixed a small issue with the knife configuration file

--- chef-repo/.chef/knife.rb
+++ chef-repo/.chef/knife.rb
@@ -4,6 +4,6 @@ node_name                'admin'
 client_key               "#{ENV['chef_repo']}/.chef/admin.pem"
 validation_client_name   'tcga-validator'
 validation_key           "#{ENV['chef_repo']}/.chef/tcga-validator.pem"
-chef_server_url          "#{ENV['chef_server_url']}"
+chef_server_url          "https://#{ENV['chef_server']}/organizations/tcga"
 syntax_check_cache_path  "#{ENV['chef_repo']}/.chef/syntax_check_cache"
 cookbook_path	[ "#{ENV['chef_repo']}/cookbooks" ]
