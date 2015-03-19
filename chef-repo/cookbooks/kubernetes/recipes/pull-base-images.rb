ca089ebf069124975da77fced9891dd809722f0b
vardaofthevalier <abigail.n.hahn@gmail.com>
2015-03-19 16:13:55 -0700

    Attempting to fix local docker registry login procedure in the kubernetes cookbook

--- chef-repo/cookbooks/kubernetes/recipes/pull-base-images.rb
+++ chef-repo/cookbooks/kubernetes/recipes/pull-base-images.rb
@@ -1,8 +1,16 @@
+# Get data bag items
+registry_users = data_bag_item("nginx_proxy_auth", "docker_registry_users")
+registry_admin = registry_users["admin_user"]
+registry_admin_password = registry_users["users"][registry_admin]
+
 # Pull the official chef base images
 docker_image "chef/ubuntu-14.04"
 
 # Get the private registry ready to push to
-#docker_registry "localhost:5000"
+docker_registry "https://#{node[:docker_registry][:environment][:registry_address]}:#{node[:docker_registry][:environment][:registry_port]}" do 
+	username "#{registry_admin}"
+	password "#{registry_admin_password}"
+end
 
 # Tag the image for pushing to the local registry
 docker_image "chef/ubuntu-14.04" do
