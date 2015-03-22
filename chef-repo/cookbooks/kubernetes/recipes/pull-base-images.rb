0f4195bf28a2e592abf494b2c9d651cfd0e52f60
vardaofthevalier <abigail.n.hahn@gmail.com>
2015-03-22 02:43:24 -0700

    Using a workaround for the docker registry (uses plain HTTP requests)

--- chef-repo/cookbooks/kubernetes/recipes/pull-base-images.rb
+++ chef-repo/cookbooks/kubernetes/recipes/pull-base-images.rb
@@ -8,11 +8,12 @@ registry_admin_email = registry_users["users"][registry_admin]["email"]
 docker_image "chef/ubuntu-14.04"
 
 # Get the private registry ready to push to
-docker_registry "#{node[:hostname]}:#{node[:docker_registry][:nginx_conf][:ssl_port]}" do 
-	username "#{registry_admin}"
-	password "#{registry_admin_password}"
-	email "#{registry_admin_email}"
-end
+#docker_registry "#{node[:hostname]}:#{node[:docker_registry][:nginx_conf][:ssl_port]}" do 
+#	username "#{registry_admin}"
+#	password "#{registry_admin_password}"
+#	email "#{registry_admin_email}"
+#end
+docker_registry "localhost:5000" 
 
 # Tag the image for pushing to the local registry
 docker_image "chef/ubuntu-14.04" do
