21b86e177529055a462e566ad5f2c42dcc46aa7c
Abigail Hahn <ahahn@systemsbiology.org>
2015-05-22 18:35:00 -0700

    Updated the .chef directory for use with the new chef server on glados51

--- chef-repo/.chef/debug/chef_shell.rb
+++
@@ -1,3 +0,0 @@
-node_name 'workstation'
-client_key File.expand_path('~/docker-prototyping/.chef/workstation.pem')
-chef_server_url 'https://chef-server-centos.com/organizations/tcga'
--- chef-repo/.chef/knife.rb
+++ chef-repo/.chef/knife.rb
@@ -1,10 +1,10 @@
 log_level                :info
 log_location             STDOUT
-node_name                'admin'
-client_key               "#{ENV['chef_repo']}/.chef/admin.pem"
-validation_client_name   'tcga-validator'
-validation_key           "#{ENV['chef_repo']}/.chef/tcga-validator.pem"
-chef_server_url          "https://#{ENV['chef_server']}/organizations/tcga"
+node_name                'tcga-admin'
+client_key               "#{ENV['chef_repo']}/.chef/tcga-admin.pem"
+validation_client_name   'isb-tcga-validator'
+validation_key           "#{ENV['chef_repo']}/.chef/isb-tcga-validator.pem"
+chef_server_url          "https://#{ENV['chef_server']}/organizations/isb-tcga"
 syntax_check_cache_path  "#{ENV['chef_repo']}/.chef/syntax_check_cache"
 cookbook_path	[ "#{ENV['chef_repo']}/cookbooks" ]
 repo_mode	'everything'
