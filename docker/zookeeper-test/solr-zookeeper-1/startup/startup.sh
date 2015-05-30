34df1bfcbb915cceed868bb1338b2f8476f19255
Abigail Hahn <ahahn@systemsbiology.org>
2015-05-29 17:25:48 -0700

    Working on a startup script for the zookeeper containers that will cause the container to add itself to a global configuration file used by all zookeepers

---
+++ docker/zookeeper-test/solr-zookeeper-1/startup/startup.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+max_zookeepers=$1
+current_zookeepers=`grep server $ZOOKEEPER_CONF/zoo.cfg | wc -l`
+
+if [[ !`grep $HOSTNAME $ZOOKEEPER_CONF/zoo.cfg` ]]; then
+	((current_zookeepers += 1))
+	if [[ `grep server.$zookeepers $ZOOKEEPER_CONF/zoo.cfg` ]]; then
+		# replace the line with the new one
+	else
+		echo "server.$zookeepers=$HOSTNAME:2888:3888" >> $ZOOKEEPER_CONF/zoo.cfg
+		echo $current_zookeepers >> $ZOOKEEPER_DATA_DIR/myid
+	fi
+fi
+
+while true; do
+	if [[ $current_zookeepers == $max_zookeepers ]]; then
+		java -cp $ZOOKEEPER_HOME/zookeeper.jar:$ZOOKEEPER_HOME/lib/slf4j-api-1.7.5.jar:$ZOOKEEPER_HOME/lib/slf4j-log4j12-1.7.5.jar:$ZOOKEEPER_HOME/lib/log4j-1.2.16.jar:$ZOOKEEPER_CONF/conf org.apache.zookeeper.server.quorum.QuorumPeerMain $ZOOKEEPER_CONF/zoo.cfg
+	else
+		current_zookeepers=`grep server $ZOOKEEPER_CONF/zoo.cfg | wc -l`
+	fi
+done
